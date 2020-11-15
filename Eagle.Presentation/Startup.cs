11fa4210f52368fd909ca9447b12ddbd9d469347
Atiol Raphael <atl.rafa@gmail.com>
2020-11-15 23:22:32 +0300

    Swagger
    Added swagger api documentation

--- Eagle.Presentation/Startup.cs
+++ Eagle.Presentation/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using Serilog;
 
 namespace Eagle.Presentation
@@ -27,6 +30,26 @@ namespace Eagle.Presentation
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c => 
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Eagle API",
+                    Description = "An API for the Eagle Project",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Atiol Raphael Beato",
+                        Email = "atl.rafa@gmail.com",
+                        Url = new Uri("https://twitter.com/atlrafa2")
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +64,12 @@ namespace Eagle.Presentation
             app.UseSerilogRequestLogging();
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Eagle API V1");
+            });
 
             app.UseRouting();
 
